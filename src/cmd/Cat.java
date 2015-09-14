704e8398ea005ddb15cffc335f29b8e23b9d1a2e
Andrey Tolmachev <andrey.tolmachev@gmail.com>
2015-09-14 21:10:53 +0300

    handle correctly directories

--- src/cmd/Cat.java
+++ src/cmd/Cat.java
@@ -7,9 +7,21 @@ import java.util.Set;
 public class Cat implements Cmd {
     @Override
     public void execute(String arg, Set<Character> options, InputStream in, PrintStream out) {
-        try (Scanner scanner = new Scanner(arg == null ? in : new FileInputStream(arg))) {
+        try (Scanner scanner = createScanner(arg, in)) {
             while (scanner.hasNextLine()) {
                 out.println(scanner.nextLine());
+            }
+        }
+    }
+
+    private Scanner createScanner(String arg, InputStream in) {
+        try {
+            if (arg != null) {
+                File file = new File(arg);
+                if (file.isDirectory()) throw new CmdFailed("%s: Is a directory", arg);
+                return new Scanner(file, "UTF-8");
+            } else {
+                return new Scanner(in);
             }
         } catch (FileNotFoundException e) {
             throw new CmdFailed("%s: No such file or directory", arg);
